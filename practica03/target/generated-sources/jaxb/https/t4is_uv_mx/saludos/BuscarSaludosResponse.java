159c022f2163951eb44fdf44b44c54863fa9ee1f
RubenRiveraMorales <89487755+RubenRiveraMorales@users.noreply.github.com>
2023-03-21 11:34:59 -0600

    Add files via upload

---
+++ practica03/target/generated-sources/jaxb/https/t4is_uv_mx/saludos/BuscarSaludosResponse.java
@@ -0,0 +1,163 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.03.21 a las 08:39:04 AM CST 
+//
+
+
+package https.t4is_uv_mx.saludos;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="saludo" maxOccurs="unbounded"&gt;
+ *           &lt;complexType&gt;
+ *             &lt;complexContent&gt;
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *                 &lt;sequence&gt;
+ *                   &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+ *                   &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *                 &lt;/sequence&gt;
+ *               &lt;/restriction&gt;
+ *             &lt;/complexContent&gt;
+ *           &lt;/complexType&gt;
+ *         &lt;/element&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "saludo"
+})
+@XmlRootElement(name = "BuscarSaludosResponse")
+public class BuscarSaludosResponse {
+
+    @XmlElement(required = true)
+    protected List<BuscarSaludosResponse.Saludo> saludo;
+
+    /**
+     * Gets the value of the saludo property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the saludo property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getSaludo().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link BuscarSaludosResponse.Saludo }
+     * 
+     * 
+     */
+    public List<BuscarSaludosResponse.Saludo> getSaludo() {
+        if (saludo == null) {
+            saludo = new ArrayList<BuscarSaludosResponse.Saludo>();
+        }
+        return this.saludo;
+    }
+
+
+    /**
+     * <p>Clase Java para anonymous complex type.
+     * 
+     * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+     * 
+     * <pre>
+     * &lt;complexType&gt;
+     *   &lt;complexContent&gt;
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+     *       &lt;sequence&gt;
+     *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/&gt;
+     *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+     *       &lt;/sequence&gt;
+     *     &lt;/restriction&gt;
+     *   &lt;/complexContent&gt;
+     * &lt;/complexType&gt;
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "id",
+        "nombre"
+    })
+    public static class Saludo {
+
+        protected int id;
+        @XmlElement(required = true)
+        protected String nombre;
+
+        /**
+         * Obtiene el valor de la propiedad id.
+         * 
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * Define el valor de la propiedad id.
+         * 
+         */
+        public void setId(int value) {
+            this.id = value;
+        }
+
+        /**
+         * Obtiene el valor de la propiedad nombre.
+         * 
+         * @return
+         *     possible object is
+         *     {@link String }
+         *     
+         */
+        public String getNombre() {
+            return nombre;
+        }
+
+        /**
+         * Define el valor de la propiedad nombre.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link String }
+         *     
+         */
+        public void setNombre(String value) {
+            this.nombre = value;
+        }
+
+    }
+
+}
